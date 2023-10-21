4f864a068e4842a59c7d76999fcf25f010e192f5
NimanthaH <m2sandeepa@gmail.com>
2023-10-22 00:19:04 +0530

    route changed

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,10 @@ const managemeetings = require('./managemeetings');
 const integrations = require('./integrations');
 const auth = require('./auth');
 
+routes.get('/', (req, res) => {
+  res.status(200).json({ message: 'brixc api connected!' });
+});
+
 routes.use('/auth', auth);
 routes.use('/settings', settings);
 routes.use('/managemeetings', managemeetings);
@@ -19,9 +23,5 @@ routes.use(authenticateToken);
 routes.use('/integrations', integrations);
 
 routes.use(express.json());
-
-routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'brixc api connected!' });
-});
 
 module.exports = routes;

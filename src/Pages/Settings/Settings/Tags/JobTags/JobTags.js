568b4b8ca854b9d1f99105b2b6baf1570fea5535
NimanthaH <m2sandeepa@gmail.com>
2023-10-22 20:52:53 +0530

    changed gitignore

--- src/Pages/Settings/Settings/Tags/JobTags/JobTags.js
+++ src/Pages/Settings/Settings/Tags/JobTags/JobTags.js
@@ -1,58 +1,45 @@
 import React, { Fragment } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { connect } from 'react-redux'; // Import connect from react-redux
 import { Row, Col } from 'reactstrap';
-import CrudTable from '../../../../../Components/Common/CrudTable'; // Import the new component
+import CrudTable from '../../../../../Components/Common/CrudTable';
+import { fetchTagData, addTag, editTag, deleteTag } from '../../../../../Store/Reducers/Data/DataManageOptions'; // Import Redux actions
 
 const itemsPerPage = 5;
 
-const initialData = [
-    { id: 1, tag: 'angry', status: true },
-    { id: 2, tag: 'fearful', status: true },
-    { id: 3, tag: 'happy', status: true },
-    { id: 4, tag: 'surprised', status: true },
-    { id: 5, tag: 'sad', status: true },
-    { id: 6, tag: 'excited', status: true },
-    // Add more data
-];
-
-export default class JobTags extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: initialData,
-            totalItems: initialData.length,
-        };
+class JobTags extends React.Component {
+    componentDidMount() {
+        // Fetch data when the component mounts
+        this.props.fetchTagData();
     }
 
     // Function to fetch data for the specified page and items per page
     fetchData = (page, itemsPerPage) => {
-        // In a real application, you would make an API call to fetch data
-        // Here, we simulate pagination by slicing the data
-        const startIndex = (page - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        const newData = initialData.slice(startIndex, endIndex);
-
-        this.setState({ data: newData });
+        // You can remove this function as it is no longer needed
     }
 
-    handleEdit = (id) => {
-        // Handle edit action here, e.g., open a modal for editing
-        console.log(`Edit item with ID ${id}`);
+    handleEdit = (tag_id) => {
+        // Dispatch the editTag action when the user edits an item
+        this.props.editTag(tag_id);
     }
 
-    handleDelete = (id) => {
-        // Handle delete action here, e.g., show a confirmation dialog
-        console.log(`Delete item with ID ${id}`);
+    handleDelete = (tag_id) => {
+        // Dispatch the deleteTag action when the user deletes an item
+        this.props.deleteTag(tag_id);
     }
 
     handleAdd = () => {
-        // Handle add action here, e.g., open a modal for adding a new item
-        console.log('Add new item');
+        // Dispatch the addTag action when the user adds a new item
+        const newItem = {
+            id: this.props.tag_data.length + 1, // You might want to generate a unique ID
+            tag: 'New Tag', // Modify as needed
+            status: true, // Modify as needed
+        };
+        this.props.addTag(newItem);
     }
 
     render() {
-        const { data, totalItems } = this.state;
+        const { tag_data, totalItems } = this.props; // Get tag_data from Redux state
 
         const headers = ['ID', 'Tag', 'Status'];
 
@@ -70,10 +57,9 @@ export default class JobTags extends React.Component {
                         <Row>
                             <Col md="12">
                                 <CrudTable
-                                    data={data}
+                                    data={tag_data} // Use data from Redux state
                                     itemsPerPage={itemsPerPage}
                                     headers={headers}
-                                    onPageChange={this.fetchData}
                                     totalItems={totalItems}
                                     onEdit={this.handleEdit}
                                     onDelete={this.handleDelete}
@@ -87,3 +73,12 @@ export default class JobTags extends React.Component {
         );
     }
 }
+
+const mapStateToProps = (state) => ({
+    tag_data: state.tag_data, // Map the tag_data from Redux state to props
+    totalItems: state.tag_data.length, // Calculate total items
+});
+
+const mapDispatchToProps = { fetchTagData, addTag, editTag, deleteTag };
+
+export default connect(mapStateToProps, mapDispatchToProps)(JobTags); // Connect the component to the Redux store
